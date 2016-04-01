d714c09056d1d7d0cfbb537972ac7e93da9928c9
FFLAB <francesco@MacBook-Air.local>
2016-04-01 14:57:36 +0200

    update

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
 	<meta property="og:image" content="<?php $media = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); echo $media; ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link href='https://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
 
 <?php wp_head(); ?>
 </head>
@@ -15,9 +16,9 @@
 <div class="demo-layout-transparent mdl-layout mdl-js-layout">
     <!-- MENU PRINCIPALE -->
     <header class="mdl-layout__header mdl-layout__header--transparent" style="background-image: url('<?php echo get_header_image(); ?>');">
-       
-       
-       
+
+
+
         <div class="mdl-layout__header-row">
             <!-- CUSTOM LOGO -->
             <?php if ( get_theme_mod( 'themeslug_logo' ) ) : ?>
@@ -27,7 +28,7 @@
             <?php else : ?>
                 <span class="mdl-layout-title uno"><a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a></span>
             <?php endif; ?>
-            
+
             <?php wp_nav_menu( array(
                     'container' => 'nav',
                     'container_class' => 'mdl-navigation',
@@ -35,7 +36,7 @@
                     'theme_location' => 'menu_principale'
                   ) ); ?>
         </div>
-    </header>    
+    </header>
     <!-- MENU SECONDARIO -->
     <?php $menu = wp_nav_menu( array(
                     'container' => 'nav',
@@ -44,7 +45,7 @@
                     'theme_location' => 'menu_secondario',
                     'echo' => FALSE,
                 'fallback_cb' => '__return_false'
-                  ) ); 
+                  ) );
     if(! empty ($menu) ){
      echo ('<header class="mdl-layout__header" id="cat-menu"><div class="mdl-layout__header-row">' . $menu . '</div></header>');
     } ?>
@@ -70,5 +71,3 @@
     <!-- FINE HEADER -->
     <main class="mdl-layout__content">
         <div class="page-content">
-            
-        
