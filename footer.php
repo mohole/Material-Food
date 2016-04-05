09258e78a113e4751ccba6bd781c1dbfe0ce63c1
FFLAB <riggio.francesco@gmail.com>
2016-04-05 17:27:04 +0200

    fix footer
    character info

--- footer.php
+++ footer.php
@@ -3,13 +3,13 @@
         <?php dynamic_sidebar("Footer"); ?>
     </div>
     <div class="mdl-mega-footer__bottom-section">
-        <div class="mdl-logo">Material Food è un tema gratuito per <a href="http://www.wordpress.org/">WordPress</a> | <a href="http://scuola.mohole.it">Scuola Mohole 2016</a>
+        <div class="mdl-logo">Material Food &#232; un tema gratuito per <a href="http://www.wordpress.org/">WordPress</a> | <a href="http://scuola.mohole.it">Scuola Mohole 2016</a>
         </div>
     </div>
 </footer>
-        
+
 <?php wp_footer(); ?>
-        
+
         </div> <!-- CHIUSURA page-content (header.php) -->
     </main> <!-- CHIUSURA mdl-layout__content (header.php) -->
 </div> <!-- CHIUSURA demo-layout-transparent (header.php) -->
