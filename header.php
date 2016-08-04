908f7ad3acdbd66d39fea9078d17fc2d6761e8c2
FFLAB <riggio.francesco@gmail.com>
2016-08-04 11:56:11 +0200

    Doctype fix

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
-<!doctype html <?php language_attributes(); ?>>
-<html lang="it">
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 <head>
     <meta charset="UTF-8">
     <title><?php wp_title(); ?></title>
